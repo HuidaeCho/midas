6dfba27802383ad824be31eb5a9c9120ec645950
Huidae Cho <grass4u@gmail.com>
2024-04-16 16:01:39 -0600

    Add inputs path

--- windows/test/test.bat
+++ windows/test/test.bat
@@ -2,14 +2,14 @@
 set PATH=..;..\lib;%PATH%
 set PROJ_LIB=..\lib
 
-mefa fdr.tif outputs\fac.tif
-mefa-lessmem fdr.tif outputs\fac-lessmem.tif
-meshed fdr.tif outlets1000.shp cat outputs\wsheds1000.tif
-meshed-lessmem fdr.tif outlets1000.shp cat outputs\wsheds1000-lessmem.tif
-melfp fdr.tif outlets1000.shp cat outputs\lfp1000.csv
-melfp -l fdr.tif outlets1000.shp cat outputs\lfp1000-lessmem.csv
-meflen fdr.tif outputs\dflen.tif
-meflen -L fdr.tif outputs\dflen-leastmem.tif
-meflen -u fdr.tif outputs\uflen.tif
-meflen -ul fdr.tif outputs\uflen-lessmem.tif
-meflen -uL fdr.tif outputs\uflen-leastmem.tif
+mefa inputs\fdr.tif outputs\fac.tif
+mefa-lessmem inputs\fdr.tif outputs\fac-lessmem.tif
+meshed inputs\fdr.tif inputs\outlets1000.shp cat outputs\wsheds1000.tif
+meshed-lessmem inputs\fdr.tif inputs\outlets1000.shp cat outputs\wsheds1000-lessmem.tif
+melfp inputs\fdr.tif inputs\outlets1000.shp cat outputs\lfp1000.csv
+melfp -l inputs\fdr.tif inputs\outlets1000.shp cat outputs\lfp1000-lessmem.csv
+meflen inputs\fdr.tif outputs\dflen.tif
+meflen -L inputs\fdr.tif outputs\dflen-leastmem.tif
+meflen -u inputs\fdr.tif outputs\uflen.tif
+meflen -ul inputs\fdr.tif outputs\uflen-lessmem.tif
+meflen -uL inputs\fdr.tif outputs\uflen-leastmem.tif
