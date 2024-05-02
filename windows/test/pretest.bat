9fa8c0eed5cf67affb0b028350444eb8045911d9
Huidae Cho <grass4u@gmail.com>
2024-05-02 11:03:28 -0600

    Merge mkdir inputs & outputs

--- windows/test/pretest.bat
+++ windows/test/pretest.bat
@@ -1,4 +1,3 @@
 @echo off
-mkdir inputs
-mkdir outputs
+mkdir inputs outputs
 for %%i in (fdr.tif outlets1000.dbf outlets1000.prj outlets1000.shp outlets1000.shx) do curl https://data.isnew.info/meidas/%%i -o inputs/%%i
