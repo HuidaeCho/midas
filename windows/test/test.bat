2208f07da3cd00fa1ddb6896fe3ed9e7dbad524d
Huidae Cho <grass4u@gmail.com>
2024-05-02 09:13:19 -0600

    Update binaries

--- windows/test/test.bat
+++ windows/test/test.bat
@@ -3,9 +3,9 @@ set PATH=..;..\lib;%PATH%
 set PROJ_LIB=..\lib
 
 mefa inputs\fdr.tif outputs\fac.tif
-mefa-lessmem inputs\fdr.tif outputs\fac-lessmem.tif
+mefa -l inputs\fdr.tif outputs\fac-lessmem.tif
 meshed inputs\fdr.tif inputs\outlets1000.shp cat outputs\wsheds1000.tif
-meshed-lessmem inputs\fdr.tif inputs\outlets1000.shp cat outputs\wsheds1000-lessmem.tif
+meshed -l inputs\fdr.tif inputs\outlets1000.shp cat outputs\wsheds1000-lessmem.tif
 melfp inputs\fdr.tif inputs\outlets1000.shp cat outputs\lfp1000.csv
 melfp -l inputs\fdr.tif inputs\outlets1000.shp cat outputs\lfp1000-lessmem.csv
 meflen inputs\fdr.tif outputs\dflen.tif
