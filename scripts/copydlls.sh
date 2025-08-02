8acd43d5a10a931d7db10075f56e945ad858669f
Huidae Cho <grass4u@gmail.com>
2025-08-02 03:52:35 -0600

    Fix copydlls.sh

--- scripts/copydlls.sh
+++ scripts/copydlls.sh
@@ -5,9 +5,9 @@ mkdir lib
 if [ -f midas.dll ]; then
 	peldd	--ignore-errors -a --no-path \
 		--path /mnt/c/opt/miniconda/envs/midas/Library/bin \
-		midas.dll | sed '/midas.dll/d' | xargs -r cp -av -t .
+		midas.dll | sed '/midas.dll/d' | xargs -r cp -av -t lib
 elif [ -f libmidas.dll ]; then
 	peldd	--ignore-errors -a --no-path \
 		--path /mnt/c/opt/miniconda/envs/midas_mingw/Library/bin \
-		libmidas.dll | sed '/libmidas.dll/d' | xargs -r cp -av -t .
+		libmidas.dll | sed '/libmidas.dll/d' | xargs -r cp -av -t lib
 fi
