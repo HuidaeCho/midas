132f7b4c7e515d8f6a4da7fae502089fe495a333
Huidae Cho <grass4u@gmail.com>
2025-08-02 03:45:58 -0600

    Delete windows/lib

--- scripts/copydlls.sh
+++ scripts/copydlls.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-# run this script from windows in WSL
+# run this script from windows/msvc or windows/mingw in WSL
 [ -d lib ] && rm -rf lib
 mkdir lib
-peldd	--ignore-errors -a --no-path \
-	--path /mnt/c/opt/miniconda/envs/midas/Library/bin \
-	msvc/midas.dll | sed '/midas.dll/d' | xargs -r cp -av -t lib
+if [ -f midas.dll ]; then
+	peldd	--ignore-errors -a --no-path \
+		--path /mnt/c/opt/miniconda/envs/midas/Library/bin \
+		midas.dll | sed '/midas.dll/d' | xargs -r cp -av -t .
+elif [ -f libmidas.dll ]; then
+	peldd	--ignore-errors -a --no-path \
+		--path /mnt/c/opt/miniconda/envs/midas_mingw/Library/bin \
+		libmidas.dll | sed '/libmidas.dll/d' | xargs -r cp -av -t .
+fi
