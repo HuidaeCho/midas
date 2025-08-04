05c8b15c76a955a701a1a6e1b6ec10929a1d0d13
Huidae Cho <grass4u@gmail.com>
2025-08-04 05:53:02 -0600

    Separate midas import and example code for Python

---
+++ python/example.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from midas import *
+
+tracing_stack_size = read_tracing_stack_size(0)
+num_threads = init_midas(0)
+
+mefa("inputs/fdr.tif",
+     None,
+     None,
+     "accum.tif",
+     num_threads=num_threads)
+
+meshed("inputs/fdr.tif",
+       None,
+       None,
+       "inputs/outlets515152.shp",
+       None,
+       None,
+       "cat",
+       "wsheds.tif",
+       num_threads=num_threads,
+       tracing_stack_size=tracing_stack_size)
+
+melfp("inputs/fdr.tif",
+      None,
+      None,
+      "inputs/outlets515152.shp",
+      None,
+      None,
+      "cat",
+      "lfp.gpkg",
+      num_threads=num_threads,
+      tracing_stack_size=tracing_stack_size)
